7141644023bfa2a8dbb1d21548d7287779948b20
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2021-08-28 02:25:28 +0600

    Add files via upload

---
+++ Comment.py
@@ -0,0 +1,13 @@
+# Comments in Python
+# Two types of Comments in Python 1.#, 2."""-------"""
+
+# First Line or One line comment
+# print("Hello World!\n")
+
+# Multiple Line Comment
+"""if 5 > 2:
+    print("5 is grater than 2") 
+    
+if 510 > 2:
+    print("510 is grater than 2") 
+   """
