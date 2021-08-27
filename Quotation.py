759bbb9d7ced09bceb727bbfdac047208a458094
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2021-08-28 02:14:57 +0600

    Add files via upload

---
+++ Quotation.py
@@ -0,0 +1,13 @@
+# Quotation in Python
+# We have three types of quotation just like 1.('A'), 2.("A"), 3.("'A'")
+# 1.Number
+if 5 > 2:
+    print('5 is grater than 2')  # just like 1.('A')
+
+# 2.Number
+if 5 > 2:
+    print("5 is grater than 2")  # just like 2.("A")
+
+# 3.Number
+if 5 > 2:
+    print("'5 is grater than 2'") # just like 3.("'A'")
