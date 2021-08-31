aca5f493f7e79bbcefa5a53daa4a3c383b25ff20
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2021-09-01 00:54:54 +0600

    Add files via upload

---
+++ Getting Input From User.py
@@ -0,0 +1,15 @@
+# Getting input form user
+
+# Integer String :
+i = (input("Enter Your Name : "))
+
+# Integer Input :
+j = int(input("Enter Your Roll Number : "))
+
+# Integer Float :
+k = float(input("Enter Your CGPA : "))
+
+# Printing all statement
+print(i)
+print(j)
+print(k)
