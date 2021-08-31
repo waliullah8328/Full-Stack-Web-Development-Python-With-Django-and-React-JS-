862ff6b65d324887d9ead8b90a25efb93dcf6494
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2021-09-01 00:36:06 +0600

    Add files via upload

---
+++ Data Type & Variable.py
@@ -0,0 +1,27 @@
+# Data Type
+"""
+String ---name,age,Qualification
+Int --- 12,-1,0,56
+Float --- 12.40,67.50,50.8
+char --- 'a','z','d'
+"""
+
+# variable
+# container,box,Bucket,memory_space
+
+i = 10  # i = variable ba container & 10 integer digit
+print(i)
+print(id(i))  # id container ar location
+
+i = 19.6  # i = variable ba container & 19.6 float digit
+print(i)
+print(id(i))  # id container ar location
+
+i = "Hi, good job!"  # i = variable ba container & "Hi, good job!" String
+print(i)
+print(id(i))  # id container ar location
+
+i = 'a'  # i = variable ba container & 'a' character
+print(i)
+print(id(i))  # id container ar location
+
