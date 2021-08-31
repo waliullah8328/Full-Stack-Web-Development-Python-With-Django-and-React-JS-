1fda0381f7a156c8a5a6ee819e6d2182ab10de51
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2021-09-01 00:43:51 +0600

    Add files via upload

---
+++ Numeric Operation.py
@@ -0,0 +1,21 @@
+# Basic Numeric Operation
+"""
+1. +
+2. -
+3. *
+4. /
+5. // (Floor)
+6. ** (Exponential)
+"""
+
+print(100+10)  # using '+' operator
+
+print(100-10)  # using '-' operator
+
+print(100*10)  # using '*' operator
+
+print(100/10)  # using '/' operator
+
+print(100//10)  # using '//' operator
+
+print(100**2)  # using '**' operator
